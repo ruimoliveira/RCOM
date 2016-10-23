ed8c01d1eb5ac03b428037b617b26ad8bfc5bdfa
ruimoliveira <ruimiguelalmeidaoliveira@gmail.com>
2016-10-23 21:59:08 +0100

    Update
    updated:
    llwrite
    added:
    getBBC
    encapsulatePacket
    stuffPacket
    receiveCommandPacket
    
    still TODO: llread & receivePacket

--- DataLink.c
+++ DataLink.c
@@ -6,6 +6,7 @@
 #include <termios.h>
 #include <unistd.h>
 #include <strings.h>
+#include <string.h>
 #include "DataLink.h"
 
 struct termios oldtio, newtio;
@@ -18,19 +19,34 @@ struct termios oldtio, newtio;
 
 #define FLAG   0x7E
 #define A      0x03
+#define C1	   0x40
+#define C0	   0x00
 #define C_SET  0x03
 #define C_UA   0x03
-#define C_RR   0x05
-#define C_REJ  0x01
 #define C_DISC 0x0B
+#define C_RR0  0x05
+#define C_RR1  0x85
+#define C_REJ0 0x01
+#define C_REJ1 0x81
+
+#define ESCAPE 0x7d
+
+#define CONTROL_PACKET_SIZE	5
+#define DATA_PACKET_SIZE 	6*sizeof(char)
 
 const unsigned char SET[] = {FLAG, A, C_SET, A^C_SET,FLAG};
 const unsigned char UA[] = {FLAG, A, C_UA, A^C_UA, FLAG};
 const unsigned char DISC[] = {FLAG, A, C_DISC, A^C_DISC, FLAG};
+const unsigned char RR0[] = {FLAG, A, C_RR0, A^C_RR0, FLAG};
+const unsigned char RR1[] = {FLAG, A, C_RR1, A^C_RR1, FLAG};
+const unsigned char REJ0[] = {FLAG, A, C_REJ0, A^C_REJ0, FLAG};
+const unsigned char REJ1[] = {FLAG, A, C_REJ1, A^C_REJ1, FLAG};
 
 typedef enum { START, FLAG_RCV, A_RCV, C_RCV, BCC_OK, STOP } ConnectionState;
 
 int flag=0, conta=0;
+
+volatile static int control = 0;
 
 void atende()                   // atende alarme
 {
@@ -202,17 +218,97 @@ int llopen(const char * path, int type){
     return fd;
 }
 
-int sendPacket(int fd, char * buffer, int length){
-	return 1;
+unsigned char getBCC(const unsigned char* buffer, int length) {
+	unsigned char BCC = 0;
+
+	for(unsigned int i = 0; i < length; i++)
+		BCC ^= buffer[i];
+
+	return BCC;
 }
 
-int receivePacket(int fd){
-	return 1;
+unsigned char * encapsulatePacket(const unsigned char * buffer, int length){
+	unsigned char* pckt = malloc(DATA_PACKET_SIZE + length);
+
+	pckt[0] = FLAG;
+	pckt[1] = A;
+
+	unsigned char BCC1;
+	if(control){
+		pckt[2] = C1;
+		BCC1 = A ^ C1;
+
+	} else {
+		pckt[2] = C0;
+		BCC1 = A ^ C0;
+	}
+
+	pckt[3] = BCC1;
+
+	memcpy(&pckt[4], buffer, length);
+
+	unsigned char BCC2 = getBCC(buffer, length);
+
+	pckt[4 + length] = BCC2;
+	pckt[5 + length] = FLAG;
+
+	return pckt;
 }
 
-int llwrite(int fd, char * buffer, int length){
+unsigned int stuffPacket(unsigned char** packet, int length){
+	unsigned int newPacketSize = length;
+
+	int i;
+	for (i = 1; i < length - 1; i++)
+		if ((*packet)[i] == FLAG || (*packet)[i] == ESCAPE)
+			newPacketSize++;
+
+	*packet = (unsigned char*) realloc(*packet, newPacketSize);
+
+	for (i = 1; i < length - 1; i++) {
+		if ((*packet)[i] == FLAG || (*packet)[i] == ESCAPE) {
+			memmove(*packet + i + 1, *packet + i, length - i);
+
+			length++;
+
+			(*packet)[i] = ESCAPE;
+			(*packet)[i + 1] ^= 0x20;
+		}
+	}
+
+	return newPacketSize;
+}
+
+int receiveCommandPacket(int fd, const unsigned char * cmd){
+	ConnectionState state = START;
+    unsigned char ch;
+	int control_field = -1;
+
+    unsigned int i;
+	for(i=0; i < CONTROL_PACKET_SIZE; i++){				// receives UA frame sent by the receiver
+		read(fd, &ch, 1);
+		int ret = stateMachine(&state, ch);
+
+		if(ret != -1)
+			control_field = ret;
+	}
+
+
+	if(state == STOP && control_field == cmd[2]){
+		printf("Successfully received command.\n");	// reading was successful and connection was established
+		return 1;
+	} else{
+		printf("Error receiving command.\n");
+		return 0;
+	}
+}
+
+int llwrite(int fd, const unsigned char * buffer, int length){
     int transfer = 1;
     int try = 0;
+
+    unsigned char* packet;
+    unsigned int packetSize;
 
     while(transfer){
         if(try == 0){
@@ -221,20 +317,29 @@ int llwrite(int fd, char * buffer, int length){
                 return 0;
             }
             
-            sendPacket(fd, buffer, length);
+            packet = encapsulatePacket(buffer, length);
+            packetSize = stuffPacket(&packet, DATA_PACKET_SIZE + length);
+            write(fd, packet, packetSize); //send Packet
             
-            //if(try++ == 1)
+            if(try++ == 1){
                 //setalarm
+            }
         }
 
-    }
 
-    if(receivePacket(fd) == C_RR) {
-        //stopalarm
-        transfer = 0;
-    } else if(receivePacket(fd) == C_REJ) {
-        //stopalarm
-        try = 0;
+
+        if(control && receiveCommandPacket(fd, RR0)){
+        	//stopalarm
+        	control--;
+            transfer = 0;
+        } else if(!control && receiveCommandPacket(fd, RR1)){
+        	//stopalarm
+        	control++;
+            transfer = 0;
+        } else {
+			//stopAlarm();
+			try = 0;
+		}
     }
 
     //stopalarm
